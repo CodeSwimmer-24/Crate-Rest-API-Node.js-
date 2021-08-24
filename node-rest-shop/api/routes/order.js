440b3304c7b9d87f5e7a8195aaa691b63e72dbd5
Fahad <fahadmahmood1200@gmail.com>
2021-08-25 00:22:21 +0530

    Added mongoDB

--- node-rest-shop/api/routes/order.js
+++ node-rest-shop/api/routes/order.js
@@ -8,8 +8,13 @@ router.get('/',(req,res,next) => {
 });
 
 router.post('/',(req,res,next) => {
+    const order = {
+        orderId: req.body.orderId,
+        price:req.body.price,
+    };
     res.status(201).json({
-        message:'Order was created'
+        message:'Order was created',
+        createdOrder: order
     })
 })
 
--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -1,11 +1,63 @@
 const express = require('express');
-
 const app = express();
-
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/order')
- 
+const orderRoutes = require('./api/routes/order');
+
+// <--------- CORS for security--------->
+app.use((req,res,next) => {
+    res.header("Access-control-Allow-Origin","*");
+    res.header(
+        "Accress-Control-Allow-Headers",
+        "Origin, X-Requested-with,Content-Type,Accept,Authorization"
+    );
+    if(req.method === "OPTIONAL"){
+        res.header('Access-Control-Allow-Method',"PUT,POST,PATCH,DELETE,GET");
+        return res.status(200).json({});
+    }
+    next();
+}); 
+// <------------------||---------------------->
+
+// DataBase (MONGODB)
+
+mongoose.connect('mongodb+srv://ProfileView:gGQcvSNCgQk5n6c@cluster0.vbnfg.mongodb.net/verification?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+.then(() => {
+    console.log(`Connection Sucessfull`)
+})
+.catch((err) => console.log(`No connection`));
+
+// -------------------||-----------------------
+
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 app.use('/products',productRoutes);
-app.use('/orders',orderRoutes)
+app.use('/orders',orderRoutes);
+
+app.use((req,res,next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error,req,res,next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
